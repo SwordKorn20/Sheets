02f112766eb70667823ccf4e9dca19bb2d1472b1
SwordKorn20 <swordkorn@live.com>
2019-01-17 01:08:02 +0000

    Update 3
    Removed unneeded bloat
    More work on auto filling in main UI (still under test)

--- launcher.py
+++ launcher.py
@@ -8,12 +8,16 @@ class Test(test.Ui_Dialog, QtWidgets.QDialog):
         self.setupUi(self)
 
     def open_file(self):
-        file_path = str(QtWidgets.QFileDialog.getOpenFileName(self, "Open Character", "", "Text Files (*.txt)")[0])
+        file_path = str(QtWidgets.QFileDialog.getOpenFileName
+                        (self,
+                         "Open Character",
+                         "",
+                         "Text Files (*.txt)")[0]
+                        )
 
         if file_path:
             with open(file_path, 'r') as file:
                 lines = file.readlines()
-                print(lines)
                 for line in lines:
                     self.label_1.setText(lines[0])
                     self.label_2.setText(lines[1])
@@ -24,6 +28,13 @@ class Test(test.Ui_Dialog, QtWidgets.QDialog):
                     self.label_7.setText(lines[6])
                     self.label_8.setText(lines[7])
                     self.label_9.setText(lines[8])
+                    inv = open(lines[9], "r")
+                inv_dat = inv.readlines()
+                for line in inv_dat:
+                    self.label_11.setText(inv_dat[0])
+                    self.label_12.setText(inv_dat[1])
+                    self.label_14.setText(inv_dat[2])
+                    self.label_16.setText(inv_dat[3])
             file.close()
         self.show()
 
@@ -46,9 +57,31 @@ class CharCreate(charCreate.Ui_Dialog, QtWidgets.QDialog):
                      self.spinBox_3.text() + '\n',
                      self.spinBox_4.text() + '\n',
                      self.spinBox_5.text() + '\n',
-                     self.spinBox_6.text() + '\n']
+                     self.spinBox_6.text() + '\n',
+                     "chars/" + self.lineEdit.text() + "_inv.txt"]
             file.writelines(lines)
             file.close()
+
+        auto_open = open("chars/" + self.lineEdit.text() + ".txt", "r")
+        auto_read = auto_open.readlines()
+        for line in auto_read:
+            auto_edit = open(auto_read[9], "w+")
+            lines = ["30" + '\n',
+                     "75" + '\n',
+                     "83" + '\n',
+                     "98"]
+        auto_edit.writelines(lines)
+        auto_edit.close()
+        auto_open.close()
+        self.lineEdit.setText("")
+        self.comboBox.setCurrentIndex(0)
+        self.comboBox_2.setCurrentIndex(0)
+        self.spinBox.setValue(0)
+        self.spinBox_2.setValue(0)
+        self.spinBox_3.setValue(0)
+        self.spinBox_4.setValue(0)
+        self.spinBox_5.setValue(0)
+        self.spinBox_6.setValue(0)
         self.close()
 
 
